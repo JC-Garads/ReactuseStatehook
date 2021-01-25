b502ef4ceacedaca8bd7ca13965a3f033082f309
JC-Garads <74990063+JC-Garads@users.noreply.github.com>
2021-01-25 13:32:42 +0800

    added something

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div />;
-}
+  const [count, setCount] = useState(0);
+  const [num, setNum] = useState(2);
+  function increase() {
+    setCount(count + 1);
+  }
 
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  function multiply() {
+    setNum(num * 2);
+  }
+
+  function divide() {
+    setNum(num / 2);
+  }
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+      <h1>{num}</h1>
+      <button onClick={multiply}>x</button>
+      <button onClick={divide}>/</button>
+    </div>
+  );
+}
 export default App;
